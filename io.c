9c404f9b81796f2c2a28398caadf1df645e2f20d
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-02-29 18:41:57 +0000

    Adicionado diretorio img e atualizado io.c pelo Anderson

--- io.c
+++ io.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-
 #include "io.h"
 
 void printHeader(void)
-{
-  printf("------------------------------\n");
-  printf("MC326 1s2008\n");
-  printf("Gerenciamento de obras de arte\n");
-  printf("------------------------------\n\n");
-}
-
+{  
+  printf("   ########################################\n");  
+  printf("   ##                                    ##\n");
+  printf("   ##          MC326 1s2008              ##\n");              
+  printf("   ##    GERENCIAMENTO DE OBRAS DE ARTE  ##\n"); 
+  printf("   ##                                    ##\n"); 
+  printf("   ########################################\n\n"); 
+  printf("=================================================\n\n");
+}  
+       
 void printMenu(void)
-{
-  printf("A) Inserir entrada no catalogo\n");
-  printf("0) Sair\n\n");
-  printf("Opcao desejada: ");
-}
+{  
+  printf("    _____________________________________\n");
+  printf("   |                                     |\n");
+  printf("   | A) Inserir entrada no catalogo      |\n");   
+  printf("   | B) Sair                             |\n");  
+  printf("   |_____________________________________|\n\n");
+  printf("   Opcao desejada: "); 
+} 
