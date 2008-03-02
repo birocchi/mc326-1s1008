645257019c839a3e7803b63684400da46f8bf09f
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-03-02 19:13:02 +0000

    Coloquei a entrada que o Birocchi fez em menuh e menu.c, modifiquei o tipo do valor em data.h e fiz a interatividade em main.c além de atualizar o Makefile e leitura de dados em  io.c

---
+++ menu.c
@@ -0,0 +1,21 @@
+void printWelcome(void)
+{ 
+  printf("\n");
+  printf("   ########################################\n"); 
+  printf("   ##                                    ##\n");
+  printf("   ##          MC326 1s2008              ##\n");
+  printf("   ##    GERENCIAMENTO DE OBRAS DE ARTE  ##\n");
+  printf("   ##                                    ##\n");
+  printf("   ########################################\n\n");
+  printf("=================================================\n\n");
+}  
+void printMenu(void)
+{  
+  printf("\n");
+  printf("    _____________________________________\n");
+  printf("   |                                     |\n");
+  printf("   |  (i)nserir entrada no catalogo      |\n");   
+  printf("   |  (s)air                             |\n");  
+  printf("   |_____________________________________|\n\n");
+  printf("   Opcao desejada: "); 
+} 
