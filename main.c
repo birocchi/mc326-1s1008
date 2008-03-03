20573ed8150729114c8dcb02d09816db1a3c4673
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-03-03 01:22:26 +0000

    Added comments..

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 int main(){
 
-  FILE *file;
-  obra_info info;
-  char c;
+  FILE *file; 
+  obra_info info; /* Holds the artwork data. */
+  char c; /* Holds the user's choice from the menus. */
 
   printWelcome();
 
@@ -15,10 +15,13 @@ int main(){
     getchar();
     switch(c){
     case '\n':
+      /* Sometimes getchar() gets the wrong letter... so try again! */
       break;
     case 'i':
+      /* Open the file for appending. */
       file = fopen("base01.dat", "a");
       read_data(&info);
+      /* read_data() leaves a '\n' behind, so getchar() gets it. */
       getchar();
       write_data(file, &info);
       printf("\nDeseja inserir mais uma entrada? (s)im, (n)ao? ");
@@ -34,6 +37,7 @@ int main(){
       fclose(file);
       break;
     case 's':
+      printf("Saindo...\n");
       return 0;
     }
     printMenu();    
