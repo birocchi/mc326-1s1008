c1c4a15febb599a6f442d0b66ee8964c7489234a
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-03-16 18:15:06 +0000

    Instead of having 'magic' numbers we now use defines.

--- data.h
+++ data.h
@@ -1,14 +1,23 @@
 #ifndef __DATA_H_
 #define __DATA_H_
 
+/* Defining the sizes of each field. */
+#define NAME 200
+#define TYPE 100
+#define AUTHOR 125
+#define YEAR 4
+#define VALUE 12
+#define IMG 9
+
+
 typedef struct
 {
-	char title[200];
-	char type[100];
-	char author[125];
+	char title[NAME];
+	char type[TYPE];
+	char author[AUTHOR];
 	int year;
 	int value;
-	char img[9];
+	char img[IMG];
 } artwork_info;
 
 #endif
