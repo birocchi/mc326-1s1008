7635a3811400ce44ebff4a708dc64464246afc9b
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-03-25 22:56:13 +0000

    Ta fazendo a tabela na memoria direito... vou melhorar ainda.

--- db.c
+++ db.c
@@ -4,6 +4,12 @@
 #include <string.h>
 
 
+void readValue(char s[], size_t length, FILE * base)
+{
+  fgets(s, length+1, base);
+
+}
+
 int getFileSize(FILE * file){
 
   if(!file){
@@ -16,22 +22,56 @@ int getFileSize(FILE * file){
 
 }
 
-int makeArrayIndex(){
+int makeArrayIndex(char **pkindex, FILE * base, int numreg){
 
+  int i;
+
+  if(!base)
+    return 1;
+
+  fseek(base, 0, SEEK_SET);
+
+  for( i = 0; i<numreg; i++){
+    readValue(pkindex[i], 200, base);
+    fseek(base, 250, SEEK_CUR);
+    sprintf(&pkindex[i][200], "%010d", i);
+  }
+
+  return 0;
 
 }
-
 
 int main(){
 
   FILE * base;
   FILE * out;
   int i, size;
+  int numreg;
+  char **pkindex;
+
+
   
   base = fopen("base01.dat", "r");
   out = fopen("out.dat", "a");
 
-  size = getFileSize(base) / REG_SIZE;
+  size = getFileSize(base);
+  
+  numreg = size / REG_SIZE;
+
+  /*aki birocchi*/  
+  pkindex = (char**)malloc(sizeof(char*) * numreg);
+
+  for( i = 0; i < numreg; i++){
+    pkindex[i] = (char*)malloc(sizeof(char) * 210);
+  }
+
+  /*ateh aki*/
+
+  makeArrayIndex(pkindex, base, numreg);
+
+  for( i = 0; i < numreg; i++){
+    printf("%s\n", pkindex[i]);
+  }
 
   return 0;
 }
