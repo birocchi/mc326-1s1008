409e5c575d4e0528c15a66a93ea354293cf662d4
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-03-26 02:13:58 +0000

    Testing only

--- db.h
+++ db.h
@@ -6,7 +6,8 @@
 int getFileSize(FILE * file);
 int cmpstring(char **p1, char **p2);
 int makeArrayPKIndex(char **pkindex, FILE * base);
-int makeFilePKIndex(char ** pkindex, FILE * out);
+int makeFilePKIndex(char ** pkindex, FILE * out, int numreg);
 int loadPkFile(char ** pkindex, FILE * pkfile);
+char ** search(char **pkindex, char * key, int numreg);
 
 #endif
