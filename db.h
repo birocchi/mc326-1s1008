1399b65b01e04cd2866f93fe99517696763b2a16
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-03-26 02:10:50 +0000

    db.h sanitization

--- db.h
+++ db.h
@@ -1,6 +1,12 @@
+#ifndef __DATA_H
+#define __DATA_H
+
+#include <stdio.h>
+
 int getFileSize(FILE * file);
 int cmpstring(char **p1, char **p2);
 int makeArrayPKIndex(char **pkindex, FILE * base);
 int makeFilePKIndex(char ** pkindex, FILE * out);
 int loadPkFile(char ** pkindex, FILE * pkfile);
 
+#endif
