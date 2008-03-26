ffd970eda8e7c2a5147c40cd98596b6f3e0e3383
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-03-26 00:18:59 +0000

    Fiz um monte de coisa... vou comer.

--- db.c
+++ db.c
@@ -22,7 +22,12 @@ int getFileSize(FILE * file){
 
 }
 
-int makeArrayIndex(char **pkindex, FILE * base, int numreg){
+int cmpstring(char **p1, char **p2){
+
+  return strcmp((const char*) *p1, (const char*) *p2);
+}
+
+int makeArrayPKIndex(char **pkindex, FILE * base, int numreg){
 
   int i;
 
@@ -37,8 +42,53 @@ int makeArrayIndex(char **pkindex, FILE * base, int numreg){
     sprintf(&pkindex[i][200], "%010d", i);
   }
 
+  qsort(pkindex, numreg, sizeof(char*), (const void *)cmpstring);
+
   return 0;
 
+}
+
+int makeFilePKIndex(char ** pkindex, FILE * out, int numreg){
+  
+  int i;
+
+  for(i = 0; i< numreg; i++){
+    fprintf(out, "%-210s", pkindex[i]);
+  }
+
+  return 0;
+}
+
+int loadPkFile(char **pkindex, FILE * pkfile){
+
+  if(!pkfile)
+    return 1;
+  
+  int size, numpk,i;
+  char file_array[210];
+
+  size = getFileSize(pkfile);
+  numpk = size/210;
+
+/*   /\* Create the pk table in the memory*\/ */
+/*   pkindex = (char**)malloc(sizeof(char*) * numpk); */
+/*   for( i = 0; i < numpk; i++){ */
+/*     pkindex[i] = (char*)malloc(sizeof(char) * 210); */
+/*   } */
+
+  /* Copy the primary keys of the pkfile and put them in the memory pk table*/
+
+  for(i=0; i<numpk; i++){
+    fgets(file_array, 211, pkfile);
+
+    /*Essa porra aqui fica lendo lixo no file_array, se eh que le alguma coisa... */
+  }
+  
+  for(i =0; i< numpk; i++){
+    printf("%s\n", pkindex[i]);
+  }
+
+  return 0;
 }
 
 int main(){
@@ -48,30 +98,52 @@ int main(){
   int i, size;
   int numreg;
   char **pkindex;
+  char **pkteste;
 
 
   
   base = fopen("base01.dat", "r");
-  out = fopen("out.dat", "a");
+  out = fopen("out.dat", "w");
 
   size = getFileSize(base);
   
   numreg = size / REG_SIZE;
 
-  /*aki birocchi*/  
+
+  /*Allocating some memory for our PK table.*/
   pkindex = (char**)malloc(sizeof(char*) * numreg);
 
   for( i = 0; i < numreg; i++){
     pkindex[i] = (char*)malloc(sizeof(char) * 210);
   }
+  /**/
 
-  /*ateh aki*/
 
-  makeArrayIndex(pkindex, base, numreg);
 
+  makeArrayPKIndex(pkindex, base, numreg);
+
+
+
+  makeFilePKIndex(pkindex, out, numreg);
+
+  /*Printing for debugging... */
   for( i = 0; i < numreg; i++){
     printf("%s\n", pkindex[i]);
   }
+
+  fclose(out);
+
+  /*Agora vem a parte de ler o arquivo na memoria.*/
+
+  out = fopen("out.dat", "r");
+
+  pkteste = (char**)malloc(sizeof(char*) * numreg);
+
+  for( i = 0; i < numreg; i++){
+    pkteste[i] = (char*)malloc(sizeof(char) * 210);
+  }
+
+  loadPkFile(pkteste, out);
 
   return 0;
 }
