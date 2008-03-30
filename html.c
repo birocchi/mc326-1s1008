e840e184f87fc71a1bdde38e27744603c085d2a1
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-03-30 22:10:14 +0000

    More linebreaks between each tag

--- html.c
+++ html.c
@@ -27,13 +27,13 @@ void htmlWriteRecordInfo(FILE* htmlfile, artwork_info* info) {
   char* path = baseGetValidImagePath(info->img);
 
   if (htmlfile && info) {
-    fprintf(htmlfile, "<img src=\"img/%s\">", path);
+    fprintf(htmlfile, "<img src=\"img/%s\">\n", path);
     fprintf(htmlfile, "<p><b>Titulo:</b> %s</p>\n", info->title);
     fprintf(htmlfile, "<p><b>Tipo:</b> %s</p>\n", info->type);
     fprintf(htmlfile, "<p><b>Autor:</b> %s</p>\n", info->author);
     fprintf(htmlfile, "<p><b>Ano:</b> %s</p>\n", info->year);
     fprintf(htmlfile, "<p><b>Valor:</b> %s</p>\n", info->value);
-    fprintf(htmlfile, "<p><b>Identificador:</b> %s</p>\n", info->img);
+    fprintf(htmlfile, "<p><b>Identificador:</b> %s</p>\n\n", info->img);
   }
 
   free(path);
