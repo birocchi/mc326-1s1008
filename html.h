6b49abb44a58fc4cb0484e1d1be1dcc08b26b2a2
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-04-02 01:28:59 +0000

    Deixando doxygen-ready.

--- html.h
+++ html.h
@@ -4,24 +4,31 @@
 #include <stdio.h>
 #include "base.h"
 
-/*
- * htmlBegin
- *
+/**
  * Writes the beginning of an HTML file to /htmlfile/.
+ *
+ * @param htmlfile The file pointer to the HTML file.
+ *
+ * @retval Returns nothing.
  */
 void htmlBegin(FILE* htmlfile);
 
-/*
- * htmlEnd
- *
+/**
  * Writes the end of an HTML file to /htmlfile/.
+ *
+ * @param htmlfile The file pointer to the HTML file.
+ *
+ * @retval Returns nothing.
  */
 void htmlEnd(FILE* htmlfile);
 
-/*
- * htmlWriteRecordInfo
- *
+/**
  * Writes the information for a single record to /htmlfile/.
+ *
+ * @param htmlfile The file pointer to the HTML file.
+ * @param info The structure that holds all the info fields for the register.
+ *
+ * @retval Returns nothing.
  */
 void htmlWriteRecordInfo(FILE* htmlfile, artwork_info* info);
 
