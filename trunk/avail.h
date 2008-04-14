4355aec880798af1caf8615c1685e5a02a59db82
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-04-14 15:58:32 +0000

    Comecei implementar a avail list da remocao da base2

---
+++ trunk/avail.h
@@ -0,0 +1,12 @@
+#ifndef __AVAIL_H
+#define __AVAIL_H
+
+#include "base.h"
+
+#define DBFILE   "base01.dat"   /* The database file name. */
+
+
+int removedField(FILE * base, int rrn, int * avail);
+
+
+#endif
