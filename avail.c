6e43c2cb97a01c8aa1c09f527da74358d98cdbed
miggaiowski <miggaiowski@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-04-14 15:58:32 +0000

    Comecei implementar a avail list da remocao da base2

---
+++ avail.c
@@ -0,0 +1,37 @@
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "base.h"
+#include "file.h"
+#include "io.h"
+#include "mem.h"
+#include "pk.h"
+#include "avail.h"
+
+int removedField(FILE * base, int rrn, int * avail){
+  int opened;
+
+  if(base){
+    opened = 1;
+    fclose(base);
+    base = fopen(DBFILE, "r+");
+  }
+  else{
+    opened = 0;
+    base = fopen(DBFILE, "r+");
+  }
+
+  fseek(base, REG_SIZE * rrn, SEEK_SET);
+  fprintf(base, "%d ", *avail);
+  *avail = rrn;
+  
+  fclose(base);
+
+  if(opened){
+    base = fopen(DBFILE, "r+");
+  }
+
+  return 0;
+
+}
