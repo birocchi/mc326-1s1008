68f54caf0d8953ad41e15c72e12992e08a20d9d7
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-04-21 22:33:56 +0000

    Finished improving pk.h documentation

--- pk.h
+++ pk.h
@@ -4,8 +4,8 @@
 #include "base.h"
 
 /** Relative record number: 32-bit integer (4 bytes) */
-#define RRN_LENGTH  4
-/** 4 bytes for a 32-bit integer */
+#define RRN_LENGTH 4
+/** Total size of a primary key entry */
 #define PK_REG_SIZE (TITLE_LENGTH + RRN_LENGTH)
 
 /**
@@ -46,45 +46,32 @@ typedef struct {
 int pkListFindByName(PrimaryKeyList* index, const char* key);
 
 /**
- * Entirely frees the struct pointed by 'index'. 
- * Frees the pointer to the list,
- * and only then frees the struct itselft.
- *
+ * \brief Free memory for a PrimaryKeyList.
  * \param index Pointer to a PrimaryKeyList struct.
  */
 void pkListFree(PrimaryKeyList* index);
 
 /**
- * Takes the pointer to the PrimaryKeyList,
- * and a string, the pk. Adds the new key
- * to the PK index.
- *
+ * \brief Inserts an entry into the list.
  * \param index Pointer the PrimaryKeyList struct.
- *
  * \param name String with the primary key.
- *
- * \return Returns 0 if everything went ok \
- * or 1 if the key was already in the list \
- * or if it had any problems inflating the table \
- * case it was already full.
+ * \retval 0 Success.
+ * \retval 1 Error.
  */
 int pkListInsert(PrimaryKeyList* index, const char* name);
 
 /**
- * Checks if the PK table is empty.
- *
+ * \brief Checks if the PK table is empty.
  * \param index Pointer to the PrimaryKeyList struct.
- *
- * \return Returns 1 if the list is empty or 'index' is NULL, 0 otherwise.
+ * \retval 0 The table is not empty.
+ * \retval 1 The table is empty.
  */
 int pkListIsEmpty(PrimaryKeyList* index);
 
 /**
  * \brief Loads data for a primary key index.
- *
  * \param base_name Database name.
  * \param pkname Primary keys index file name.
- *
  * \return Returns a new index with the loaded data,
  *         or an empty index if neither \a base_name
  *         nor \a pkname are valid files.
@@ -93,59 +80,43 @@ PrimaryKeyList* pkListLoad(const char* base_name, const char* pkname);
 
 /**
  * \brief Load the primary keys from the database.
- *
  * \param base_name The name of the database.
- *
  * \return New index structure with the loaded data.
  */
 PrimaryKeyList* pkListLoadFromBase(const char* base_name);
 
 /**
- * \brief Load the primary keys from a previous
- *        primary keys cache.
- *
+ * \brief Load the primary keys from a previous primary keys cache.
  * \param pkname The name of the cache file.
- *
  * \return New index structure with the loaded data.
  */
 PrimaryKeyList* pkListLoadFromPK(const char* pkname);
 
 /**
- * Allocate memory for a new primary key list and
- * initialize it.
+ * \brief Initialize a new primary keys list.
+ * \param nelem Number of elements to allocate memory for.
+ * \return Returns a pointer to the initialized list or NULL
+ *         if it could not be initialized.
  *
- * It allocates space for \a nelem records, or
+ * This function allocates space for \a nelem records, or
  * 40 records if \a nelem is 0.
- *
- * @param nelem Number of elements to allocate memory for.
- *
- * \retval Returns a pointer to the initialized list or NULL
- * if it could not be initialized.
  */
 PrimaryKeyList* pkListNew(size_t nelem);
 
 /**
- * Removes an entry from the Primary Key Index.
- *
+ * \brief Removes an entry from the primary key index.
  * \param index Pointer to the PrimaryKeyList struct.
- *
  * \param name The name (primary key) that will be removed.
- *
- * \return rrn is gonna be the removed rrn. 
- *
+ * \param rrn The removed RRN.
  * \retval 0 If removal went ok.
- *
  * \retval 1 If an error occurred.
  */
 int pkListRemove(PrimaryKeyList* index, const char* name, int* rrn);
 
 /**
- * Takes the primary key index 'index' and writes it properly
- * to the file 'pkfile'.
- *
+ * \brief Dump an index to a file.
  * \param index Pointer to the PrimaryKeyList struct.
- *
- * \param pkfile File pointer to the primary keys file.
+ * \param pkfile File where the data is going to be dumped.
  */
 void pkListWriteToFile(PrimaryKeyList* index, FILE* pkfile);
 
