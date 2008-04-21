4a097de1652815938dc00f540353737b6405756d
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-04-21 09:47:29 +0000

    Trying to use assert to check for runtime errors

--- mem.c
+++ mem.c
@@ -1,14 +1,11 @@
+#include <assert.h>
 #include <malloc.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 void* __memAllocate(size_t numelem, size_t elemsize) {
   void* chunk = calloc(numelem, elemsize); /* Aloc a new array. */
 
-  if (chunk == NULL) { /* If it didn't aloc, return error. */
-    fprintf(stderr, "Erro ao alocar %d bytes. Abortando programa.\n", numelem*elemsize);
-    exit(EXIT_FAILURE);
-  }
+  assert(chunk != NULL);
 
   return chunk;
 }
