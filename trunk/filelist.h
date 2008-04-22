5cab59c8d7f7fe928f9a81406b41a894e8aac4ab
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-04-22 03:26:49 +0000

    Movendo os nomes de arquivos para filelist.h, para que quando forem acrescentados novos main.c nao fique gigante

---
+++ trunk/filelist.h
@@ -0,0 +1,9 @@
+#ifndef __FILELIST_H
+#define __FILELIST_H
+
+#define AVAIL_HEAD  "availhead.al" /**< The avail list head file name. */
+#define DBFILE      "base01.dat"   /**< The database file name. */
+#define HTMLFILE    "lista.html"   /**< The HTML output. */
+#define PKFILE      "pkfile.pk"    /**< The primary key file name. */
+
+#endif
