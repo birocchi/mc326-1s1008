955b644ceb01ed887440a13184d2b0743037e901
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-04-23 12:40:22 +0000

    Pensando no adapter

--- branches/secondary_index/adapter.c
+++ branches/secondary_index/adapter.c
@@ -4,4 +4,18 @@ typedef struct {
   SecondaryIndex*   author_index;
   SecondaryIndex*   type_index;
   SecondaryIndex*   year_index;
-};
+} Adapter;
+
+insert
+{
+  /*
+   * 1. base_input_data
+   * 2. pk_find_data(base.title) == false
+   * 3. pk_insert(base.all)
+   * 4. author_index.insert
+   * 5. type_index.insert
+   * 6. year_index.insert
+   *
+   * Isso inclui mover readData pra base.c e transformar em base_input_data
+   */
+}
