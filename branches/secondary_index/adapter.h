0ab59ae3c7e40d6a0878a132a658a20c9081b79d
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-04-28 02:26:31 +0000

    Added adapter.h

---
+++ branches/secondary_index/adapter.h
@@ -0,0 +1,24 @@
+#ifndef __ADAPTER_H
+#define __ADAPTER_H
+
+#include "base.h"
+#include "memindex.h"
+#include "secindex.h"
+
+typedef struct {
+  Base              *base;
+  MemoryIndex       *pk_index;
+  SecondaryIndex    *author_index;
+  SecondaryIndex    *type_index;
+  SecondaryIndex    *year_index;
+} Adapter;
+
+void adapter_find (Adapter *db);
+void adapter_free (Adapter *db);
+void adapter_insert (Adapter *db);
+void adapter_list (Adapter *db);
+void adapter_load_files (Adapter *db);
+Adapter* adapter_new (void);
+void adapter_remove (Adapter *db);
+
+#endif
