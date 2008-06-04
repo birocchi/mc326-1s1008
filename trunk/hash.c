f9ec76bf7d08e068f49818f42f897f6e6acf8173
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-06-04 03:30:57 +0000

    Initial work on hash tables

--- trunk/hash.c
+++ trunk/hash.c
@@ -1,1 +1,32 @@
+#include <assert.h>
+#include <stdio.h>
 #include "hash.h"
+#include "io.h"
+
+unsigned int
+hash_function (char *key)
+{
+  unsigned int retval;
+
+  while (*key)
+    {
+      retval *= 0x01000193;
+      retval ^= *key++;
+    }
+
+  return retval % HASH_FILE_NUM;
+}
+
+char *
+hash_get_filename (const char *prefix, unsigned int hashnum)
+{
+  char filename[255]; /* 255 bytes is usually the maximum length of a filename */
+  int count;
+
+  assert (hashnum < HASH_FILE_NUM);
+
+  count = sprintf (filename, "%s.h%04u", prefix, hashnum);
+  assert (count <= 255);
+
+  return str_dup (filename);
+}
