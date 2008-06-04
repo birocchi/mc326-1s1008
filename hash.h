867b5507fa7f4a840537f08bf872b61c33b0608b
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-06-04 03:30:57 +0000

    Initial work on hash tables

--- hash.h
+++ hash.h
@@ -1,6 +1,9 @@
 #ifndef __HASH_H
 #define __HASH_H
 
-#define HASH_FILES_NUM 5 /**< Number of files the memory index is split into */
+#define HASH_FILE_NUM 1 /**< Number of files the memory index is split into */
+
+unsigned int hash_function (char *key);
+char *hash_get_filename (const char *prefix, unsigned int hashnum);
 
 #endif
