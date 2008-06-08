5d3e256e6ff3d2ed01a2a041977d0a1a1abbbb35
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-06-08 21:50:56 +0000

    Doxyfied hash.h

--- hash.h
+++ hash.h
@@ -3,7 +3,25 @@
 
 #define HASH_FILE_NUM 5 /**< Number of files the memory index is split into */
 
+/**
+ * @brief The default hash function (Fowler/Noll/Vo algorithm)
+ *
+ * @param key The key to be hashed.
+ *
+ * @return The result of the hash function modulo \a HASH_FILE_NUM.
+ */
 unsigned int hash_function (char *key);
+
+/**
+ * @brief Returns a name with the ".hXXXX" suffix appended to it.
+ *
+ * @param prefix  The base filename.
+ * @param hashnum The number to append to the filename.
+ *
+ * @return The filename with the suffix appended to it.
+ *
+ * Remember to free() the name returned by this function.
+ */
 char *hash_get_filename (const char *prefix, unsigned int hashnum);
 
 #endif
