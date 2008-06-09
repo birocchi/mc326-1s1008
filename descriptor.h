c5ffa95c91a9467556d4ac1de0f26d081b42811e
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-06-09 20:22:04 +0000

    Working descriptor implementation (BEM PORCO, ARQUITETURA SEM TRABALHAR AINDA, ALGUNS MEMORY LEAKS)

---
+++ descriptor.h
@@ -0,0 +1,39 @@
+#ifndef __DESCRIPTOR_H
+#define __DESCRIPTOR_H
+
+#include <stdlib.h>
+#include "base.h"
+#include "memindex.h"
+
+#define DESC_HASH_NUM 9
+
+typedef struct {
+  double similarity;
+  char img[IMG_LENGTH + 1];
+} SimilarityRecord;
+
+typedef struct {
+  size_t maxregs;
+  size_t regnum;
+  SimilarityRecord *list;
+} SimilarityList;
+
+typedef struct {
+  char *fp_name;
+  FILE *fp;
+  unsigned int loaded_file;
+} Descriptor;
+
+void
+descriptor_find (Descriptor *desc, char *imgname, MemoryIndex *pk, FILE *base_fp, size_t maxresults);
+
+void
+descriptor_free (Descriptor *desc);
+
+void
+descriptor_insert (Descriptor *desc, const char *pkname, unsigned char d);
+
+Descriptor *
+descriptor_new (const char *fp_name);
+
+#endif
