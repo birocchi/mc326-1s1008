e1329dc676aeb5e590ec1c816c317b668db2714d
kubito <kubito@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-06-09 20:43:25 +0000

    GNU indentation

--- trunk/descriptor.h
+++ trunk/descriptor.h
@@ -7,33 +7,34 @@
 
 #define DESC_HASH_NUM 9
 
-typedef struct {
+typedef struct
+{
   double similarity;
   char img[IMG_LENGTH + 1];
 } SimilarityRecord;
 
-typedef struct {
+typedef struct
+{
   size_t maxregs;
   size_t regnum;
   SimilarityRecord *list;
 } SimilarityList;
 
-typedef struct {
+typedef struct
+{
   char *fp_name;
   FILE *fp;
   unsigned int loaded_file;
 } Descriptor;
 
 void
-descriptor_find (Descriptor *desc, char *imgname, MemoryIndex *pk, FILE *base_fp, size_t maxresults);
+descriptor_find (Descriptor * desc, char *imgname, MemoryIndex * pk,
+                 FILE * base_fp, size_t maxresults);
 
-void
-descriptor_free (Descriptor *desc);
+void descriptor_free (Descriptor * desc);
 
-void
-descriptor_insert (Descriptor *desc, const char *pkname, unsigned char d);
+void descriptor_insert (Descriptor * desc, const char *pkname, unsigned char d);
 
-Descriptor *
-descriptor_new (const char *fp_name);
+Descriptor *descriptor_new (const char *fp_name);
 
 #endif
