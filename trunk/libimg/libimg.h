d02c6450a9435fbf6956c0c20ab5454015c33eb5
anderson.birocchi <anderson.birocchi@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-06-18 23:28:18 +0000

    voltei os arquivos da r535 exceto o relatorio

--- trunk/libimg/libimg.h
+++ trunk/libimg/libimg.h
@@ -1,4 +1,3 @@
-
 /* victor matheus de araujo oliveira
    Ra072589
    grupo 10
@@ -12,13 +11,13 @@
 #ifndef LIBIMG_H
 #define LIBIMG_H
 
-/*Definicao do tipo Imagem*/
+/* Definicao do tipo Imagem */
 typedef struct imagem
 {
-  /*Largura, altura e numero de canais*/
+  /* Largura, altura e numero de canais */
   unsigned int w,h,c;
 
-  /*Dados: NUNCA ACESSE DIRETAMENTE - use le_pixel/escreve_pixel*/
+  /* Dados: NUNCA ACESSE DIRETAMENTE - use le_pixel/escreve_pixel */
   int* r;
   int* g;
   int* b;
@@ -38,9 +37,6 @@ typedef struct nh
   double b[256];
 } NHist;
 
-/*Aloca memoria e le o png do arquivo.
-Em caso de erro, largura=altura=canais=0
-*/
 Imagem* Imagem_le(char* nome_arq_png);
 
 NHist NormalizaHistograma(Hist h);
@@ -50,16 +46,13 @@ Imagem* le_jpeg(char*);
 Imagem* le_gif(char*);
 
 Hist faz_hist(Imagem*);
-char CalculaDescritor(char* NomeImagem);
+unsigned char CalculaDescritor(char* NomeImagem);
 double ComputaSimilaridade(char* im1,char* im2);
 
-/*Funcao que libera a memoria alocada para a imagem por Imagem_le*/
 void libera_memoria(Imagem** im);
 
-/*Funcao que le os valores R G B de um pixel de uma imagem*/
 void le_pixel(Imagem* im,unsigned int x,unsigned int y,int* r,int* g,int* b);
 
-/*Funcao que escreve os valores R G B de um pixel de uma imagem*/
 void escreve_pixel(Imagem* im,unsigned int x, unsigned int y,int r, int g, int b);
 
 #endif
