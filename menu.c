b50323e80f637451fd075fa864dc2075978f3f9f
anderson.birocchi <anderson.birocchi@1683d1aa-3b47-0410-8964-61eb8f961b87>
2008-07-09 01:28:06 +0000

    Agora os menus.c e menu.h

---
+++ menu.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#include "io.h"
+#include "menu.h"
+
+char
+menuMultipleAnswers (const char *msg, const char *optstring)
+{
+  char c;
+
+  while (1)
+    {
+      printf (msg);
+
+      if ((read_char (&c)) || (!strchr (optstring, c)))
+        {
+          printf ("\n   Opcao invalida.\n");
+          continue;
+        }
+
+      return c;
+    }
+}
+
+/*int
+menuYesOrNo (const char *msg)
+{
+  return menuMultipleAnswers (msg, "sn") == 's';
+}*/
+
+/*void
+printConsultMenu (void)
+{
+  printf ("\n");
+  printf ("   ------------------------------------\n");
+  printf ("   Efetuar consulta por:\n");
+  printf ("     (a)no da obra\n");
+  printf ("     a(u)tor da obra\n");
+  printf ("     t(i)po da obra\n");
+  printf ("     (t)itulo da obra\n");
+  printf ("\n");
+  printf ("   A busca sera por apenas uma palavra.\n");
+  printf ("   ------------------------------------\n");
+  printf ("\n");
+}*/
+
+/*void
+print_search_type_menu (void)
+{
+  printf ("\n");
+  printf ("   +++++++++++++++++++++++++++++\n");
+  printf ("   Tipo de consulta:\n");
+  printf ("   (e)xata, por titulo\n");
+  printf ("   (p)arcial, por qualquer campo\n");
+  printf ("   (s)imilaridade\n");
+  printf ("   +++++++++++++++++++++++++++++\n");
+  printf ("\n");
+}*/
+
+void
+printMainMenu (void)
+{
+    printf("\n");
+    printf("==================\n");
+    printf("  #############\n");
+    printf("  # (I)nserir #\n");
+    printf("  # (R)emover #\n");
+    printf("  # (B)uscar  #\n");
+    printf("  # (S)air    #\n");
+    printf("  #############\n");
+    printf("==================\n");
+}
+
+void
+printWelcome (void)
+{
+  printf ("\n");
+  printf ("   ########################################\n");
+  printf ("   ##                                    ##\n");
+  printf ("   ##          MC326 1s2008              ##\n");
+  printf ("   ##    GERENCIAMENTO DE OBRAS DE ARTE  ##\n");
+  printf ("   ##                                    ##\n");
+  printf ("   ########################################\n\n");
+  printf ("=================================================\n");
+}
